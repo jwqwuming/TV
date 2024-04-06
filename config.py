05faf9883674dea9b645e0efde3bfc7dd14f2b01
A_C <jwqwuming@Gmail.com>
2024-04-06 19:51:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,7 @@
 source_file = "demo.txt"
 final_file = "result.txt"
 favorite_list = [
-    "广东珠江",
-    "开平综合",
-    "开平生活",
-    "CCTV-1",
-    "CCTV-5",
-    "CCTV-5+",
-    "CCTV-13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
-    "浙江卫视",
+   "CCTV-2",
     "湖南卫视",
     "翡翠台",
 ]
@@ -21,4 +11,4 @@ urls_limit = 15
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "ipv6"
